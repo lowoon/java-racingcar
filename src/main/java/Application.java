55a325c7547802d55a03a9fac6eae6083c8638f0
lowoon <sangbo0229@gmail.com>
2020-06-25 15:47:32 +0900

    feat : Application
    - InputView
    - OutputView

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
+import domain.Cars;
+import domain.RandomStrategy;
+import view.InputView;
+import view.OutputView;
 
 public class Application {
+    public static final int LIMIT = 10;
+
+    public static void main(String[] args) {
+        String inputNames = InputView.receiveCarNames();
+        Cars cars = Cars.of(inputNames, new RandomStrategy(LIMIT));
+        int tryCount = Integer.parseInt(InputView.receiveTryCount());
+
+        OutputView.printResult();
+        for (int i = 0; i < tryCount; i++) {
+            cars.race();
+            OutputView.printEachResult(cars.getCars());
+        }
+
+        OutputView.printWinners(cars.findWinners());
+    }
 }
--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,4 +1,30 @@
 package view;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import domain.Car;
+
 public class OutputView {
+    public static void printResult() {
+        System.out.println("실행 결과");
+    }
+
+    public static void printEachResult(List<Car> cars) {
+        for (Car car : cars) {
+            System.out.println(car.getName().getValue() + " : " +
+                Stream.generate(() -> "-")
+                    .limit(car.getPosition().getValue())
+                    .collect(Collectors.joining("")));
+        }
+        System.out.println();
+    }
+
+    public static void printWinners(List<Car> winners) {
+        String winnerNames = winners.stream()
+            .map(car -> car.getName().getValue())
+            .collect(Collectors.joining(", "));
+        System.out.println(winnerNames + "가 최종 우승했습니다.");
+    }
 }
